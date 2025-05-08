14f89f707f6ea0e05f7d5a3c86665bdbe04b9edb
devitadwiindayani <devitadwiindayani@gmail.com>
2025-05-08 10:21:49 +0700

    merapikan code

--- singleLinkedList.cpp
+++ singleLinkedList.cpp
@@ -2,52 +2,53 @@
 #include <string>
 using namespace std;
 
-class Node {
-    public:
+class Node
+{
+public:
     int noMhs;
     Node *next;
 };
 
-class List 
+class List
 {
     Node *START;
 
-    public:
-    List() {
+public:
+    List()
+    {
         START = NULL;
     }
 
-
-void addNode() 
-{
-    int nim;
-    cout << "\nMasukkan Nomor Mahasiswa : ";
-    cin >> nim;
-
-    Node *nodeBaru = new Node;
-    nodeBaru->noMhs = nim;
-
-    if (START == NULL || nim <= START->noMhs)
+    void addNode()
     {
-        if ((START != NULL ) && (nim == START ->noMhs))
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
         {
-            cout << "\nDuplikasi noMhs tidak diijinkan\n";
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            nodeBaru->next = START;
+            START = nodeBaru;
             return;
         }
-        nodeBaru->next = START;
-        START = nodeBaru;
-        return;
-    }
-    Node *previous = START;
-    Node *current = START;
+        Node *previous = START;
+        Node *current = START;
 
-    while ((current != NULL) && (nim >= current->noMhs))
-    {
-        if (nim == current->noMhs)
+        while ((current != NULL) && (nim >= current->noMhs))
         {
-            cout << "\nDuplikasi noMhs tidak diijinkan\n";
-            return;
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
         }
-        previous = current;
-        current = current->next;
-    }
